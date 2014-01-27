98a2a95044eca3e4ed4ce248a3ffab2ced5dad92
Joe Tamburro <joetamburro38@gmail.com>
2014-01-27 10:46:46 -0500

    removed logo and added smooth scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,14 @@
-console.log('\'Allo \'Allo!');
-
-$('.box').click(function(){
-  $(this).hide(1000).show(1000);
-})
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1200);
+        return false;
+      }
+    }
+  });
+});
